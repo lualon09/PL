146ca3def728070a0da43dae7260867acfe625ba
lualon09 <lualon09@ucm.es>
2024-03-05 18:39:21 +0100

    Primera version del Lexico de HaskPluss

---
+++ AnalizadorSintacticoCUP/alex/ALexOperations.java
@@ -0,0 +1,191 @@
+package alex;
+
+import asint.ClaseLexica;
+
+public class ALexOperations {
+  private AnalizadorLexicoTiny alex;
+  public ALexOperations(AnalizadorLexicoTiny alex) {
+   this.alex = alex;   
+  }
+  public UnidadLexica unidadId() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.IDEN,
+                                       alex.lexema()); 
+   } 
+   public UnidadLexica unidadTipoInt() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.TIPOINT); 
+   }
+   public UnidadLexica unidadTipoBool() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.TIPOBOOL); 
+   }
+   public UnidadLexica unidadTipoVoid() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.VOID); 
+   } 
+   public UnidadLexica unidadEnt() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.ENT, alex.lexema()); 
+   }  
+   public UnidadLexica unidadTrue() {
+      return new UnidadLexica(alex.fila(),alex.columna(), ClaseLexica.TRUE, alex.lexema());
+   }
+   public UnidadLexica unidadFalse() {
+      return new UnidadLexica(alex.fila(),alex.columna(), ClaseLexica.FALSE, alex.lexema());
+   }
+   public UnidadLexica unidadSuma() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.MAS); 
+   } 
+   public UnidadLexica unidadResta() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.MENOS); 
+   } 
+   public UnidadLexica unidadMul() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.POR); 
+   } 
+   public UnidadLexica unidadDiv() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.DIV); 
+   }
+   public UnidadLexica unidadMod() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.MOD); 
+   }
+   public UnidadLexica unidadSumLista() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.SUMLIST); 
+   }
+   public UnidadLexica unidadProdLista() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.PRODLIST); 
+   }
+   public UnidadLexica unidadConcat() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.CONCAT); 
+   }
+   public UnidadLexica unidadMayor() {
+   return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.MAYOR);
+   }
+   public UnidadLexica unidadMenor() {
+   return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.MENOR);
+   }
+   public UnidadLexica unidadMayorIgual() {
+   return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.MAYORIGUAL);
+   }
+   public UnidadLexica unidadMenorIgual() {
+   return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.MENORIGUAL);
+   }
+   public UnidadLexica unidadDistinto() {
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.DISTINTO);
+   }
+   public UnidadLexica unidadIgualdad() {
+   return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.IGUALDAD);
+   }
+   public UnidadLexica unidadAnd() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.AND);
+   } 
+   public UnidadLexica unidadOr() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.OR);
+   }
+   public UnidadLexica unidadNegacion() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.NEGACION);
+   }
+   public UnidadLexica unidadPAp() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.PAP); 
+   } 
+   public UnidadLexica unidadPCierre() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.PCIERRE); 
+   }
+   public UnidadLexica unidadCAp() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.CAP); 
+   } 
+   public UnidadLexica unidadCCierre() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.CCIERRE); 
+   }
+   public UnidadLexica unidadLLAp() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.LLAP); 
+   } 
+   public UnidadLexica unidadLLCierre() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.LLCIERRE); 
+   }  
+   public UnidadLexica unidadIgual() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.IGUAL); 
+   } 
+   public UnidadLexica unidadComa() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.COMA); 
+   }
+   public UnidadLexica unidadPuntoYComa() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.PUNTOYCOMA); 
+   }
+   public UnidadLexica unidadWhile() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.WHILE);
+   }
+   public UnidadLexica unidadFor() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.FOR);
+   }
+   public UnidadLexica unidadRepeat(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.REPEAT);
+   }
+   public UnidadLexica unidadIf() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.IF);
+   }
+   public UnidadLexica unidadElse() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.ELSE);
+   }
+   public UnidadLexica unidadStruct() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.STRUCT);
+   }
+   public UnidadLexica unidadSwitch() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.SWITCH);
+   }
+   public UnidadLexica unidadValueFor() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.VALUEFOR);
+      }
+   public UnidadLexica unidadCase() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.CASE);
+   }
+   public UnidadLexica unidadBreak() {
+      return new UnidadLexica(alex.fila(),alex.columna(), ClaseLexica.BREAK);
+   }
+   public UnidadLexica unidadDefault() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.DEFAULT);
+   }
+   public UnidadLexica unidadReturn() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.RETURN);
+   }
+   public UnidadLexica unidadShow() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.SHOW);
+   }
+   public UnidadLexica unidadRead() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.READ);
+   }
+   public UnidadLexica unidadMain() {
+   return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.MAIN);
+   }
+   public UnidadLexica unidadEof() {
+      return new UnidadLexica(alex.fila(),alex.columna(),ClaseLexica.EOF); 
+   }
+   public UnidadLexica unidadAmpersand(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.AMPERSAND);
+   }
+   public UnidadLexica unidadAlmohadilla(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.ALMOHADILLA);
+   }
+   public UnidadLexica unidadComillasDobles(){
+      return new UnidadLexica(alex.fila(),  alex.columna(), ClaseLexica.COMILLASDOBLES);
+   }
+   public UnidadLexica unidadPunto(){
+      return new UnidadLexica(alex.fila(),  alex.columna(), ClaseLexica.PUNTO);
+   }
+   public UnidadLexica unidadDosPuntos(){
+      return new UnidadLexica(alex.fila(),  alex.columna(), ClaseLexica.DOSPUNTOS);
+   }
+   public UnidadLexica unidadArroba(){
+      return new UnidadLexica(alex.fila(),  alex.columna(), ClaseLexica.ARROBA);
+   }
+   public UnidadLexica unidadTypedef(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.TYPEDEF);
+   }
+   public UnidadLexica unidadFlecha(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.FLECHA);
+   }
+   public UnidadLexica unidadBarra(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.BARRA);
+   }
+   public UnidadLexica unidadPuntos(){
+      return new UnidadLexica(alex.fila(), alex.columna(), ClaseLexica.PUNTOS);
+   }
+   public void error() {
+      System.err.println("***"+alex.fila()+", "+alex.columna()+" Caracter inesperado: "+alex.lexema());
+   }
+}
---
+++ ejecutar_analizadores.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ejecutar JFlex
+java -cp jflex.jar jflex.Main AnalizadorLexicoTiny.l
+
+# Ejecutar CUP
+java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions NumListLang.cup
+
+# Cambiar al directorio del analizador sintáctico CUP
+cd AnalizadorSintacticoCUP
+
+# Mover los archivos generados a las carpetas correspondientes
+mv ../AnalizadorSintacticoTiny.java asint/
+mv ../ClaseLexica.java asint/
+mv ../AnalizadorLexicoTiny.java alex/
+
+# Compilar archivos java generados por CUP
+javac -cp "../cup.jar" */*.java
+
+# Ejecutar el analizador sintáctico
+java -cp ".:../cup.jar" asint.Main ../inputList.txt
