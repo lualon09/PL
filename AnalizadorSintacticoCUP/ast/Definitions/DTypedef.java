0b245d0132bec065bc65df35af89dc72a3a3dca3
lualon09 <lualon09@ucm.es>
2024-03-20 17:25:25 +0100

    typedef instead of alias

--- AnalizadorSintacticoCUP/ast/Definitions/DTypedef.java
+++ AnalizadorSintacticoCUP/ast/Definitions/DTypedef.java
@@ -2,19 +2,19 @@ package ast.Definitions;
 
 import ast.Types.Type;
 
-public class DAlias extends D {
+public class DTypedef extends D {
     
     private Type t;
     private String name;
 
-    public DAlias(String name, Type t){
+    public DTypedef(String name, Type t){
         this.name = name;
         this.t = t;
     }
 
     @Override
     public String toString() {
-        return "alias "+ name.toString() + "=" + t.toString();
+        return "typedef "+ name.toString() + "=" + t.toString();
     }
 
     @Override
