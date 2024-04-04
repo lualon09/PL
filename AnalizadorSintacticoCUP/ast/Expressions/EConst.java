ad4b84a6cdf35b890b272c7046e64ce4dc4fefaa
lualon09 <lualon09@ucm.es>
2024-04-04 18:06:33 +0200

    arreglado EConst

--- AnalizadorSintacticoCUP/ast/Expressions/EConst.java
+++ AnalizadorSintacticoCUP/ast/Expressions/EConst.java
@@ -4,11 +4,19 @@ import ast.Types.T;
 
 public class EConst extends E{ //Expresion Constante
     private T type;
+    private E exp;
     private String value;
+
+    public EConst(E exp, T t){ //cuando es el tamaño de un array
+        this.type = t;
+        this.exp = exp;
+        this.value = "";
+    }
 
     public EConst(String value, T t){ //cuando es el tamaño de un array
         this.type = t;
         this.value = value;
+        this.exp = null;
     }
 
     @Override
@@ -17,6 +25,9 @@ public class EConst extends E{ //Expresion Constante
     }
 
     public String toString(){
-        return "(" + type.toString() + ":" + value + ")"; //las constantes no tienen tipo porque solo pueden ser enteras
+        if(exp != null) {
+            return "(" + type.toString() + ":" + exp.toString() + ")"; //las constantes no tienen tipo porque solo pueden ser enteras
+        }
+        return "(" + type.toString() + ":" + value.toString() + ")";
     }
 }
