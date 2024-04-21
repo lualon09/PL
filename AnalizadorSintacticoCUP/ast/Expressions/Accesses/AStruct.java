f9d4015e2b8ade7f70d84f96bb8a73a20d4479a4
lualon09 <lualon09@ucm.es>
2024-04-21 17:01:09 +0200

    intento de arreglo de los structs

--- AnalizadorSintacticoCUP/ast/Expressions/Accesses/AStruct.java
+++ AnalizadorSintacticoCUP/ast/Expressions/Accesses/AStruct.java
@@ -7,6 +7,7 @@ import ast.Instructions.IDeclaration;
 import ast.Types.TStruct;
 import ast.Definitions.DStruct;
 import ast.ASTNode;
+import ast.Program;
 
 public class AStruct extends A {
     private A access;
@@ -29,6 +30,7 @@ public class AStruct extends A {
 
     public void type() throws TypingException {
         access.type();
+        System.out.println("soy el acceso del struct " + access.toString());
         // comprobamos si access es de tipo struct 
         if(access.getType().kind().equals(KindT.STRUCT)) {
             boolean encontrado = false;
@@ -37,7 +39,17 @@ public class AStruct extends A {
             List<IDeclaration> fields = ((DStruct) bindNode.getType().bindNode).getFields();
             System.out.println("Los campos son: " + fields.toString() + " y voy a buscar " + field);
             for(IDeclaration f: fields){
-                if(f.getName().equals(field)) { //Comprobamos si es igual a algun campo
+                // if(f.getName().equals(field) && f.getType().kind().equals(KindT.STRUCT)){
+                //     try{
+                //         System.out.println("detecto que soy " + f.getName() + " y tengo el tipo " + f.getType().toString());
+                //         f.getType().bind();
+                //         this.bindNode = f.getType().bindNode;
+                //         System.out.println("soy " + f.getName() + " y mi binding node ahora es " + this.bindNode.toString());
+                //     }catch(BindingException b){
+                //         throw new TypingException("Struct name not found");
+                //     }
+                // }
+                if(f.getName().equals(field)) { //Comprobamos si es igual a algun campo                    
                     System.out.println("Soy el campo " + f.getName() + "y le estoy asignando tipo " + f.getType());
                     setType(f.getType());
                     encontrado = true;
