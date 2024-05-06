38b03b9da11b1e4c884bce30c3d510b80f90002e
lualon09 <lualon09@ucm.es>
2024-05-06 20:00:59 +0200

    mas bonito el directorioi

--- AnalizadorSintacticoCUP/compilar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd asint
-java -cp ../../cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions Tiny.cup
-cd ..   
-javac -cp "../cup.jar:." */*.java
--- AnalizadorSintacticoCUP/script_errores.sh
+++
@@ -1,7 +0,0 @@
-cd asint
-java -cp ../../cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions Tiny.cup
-cd .. 
-javac -cp "../cup.jar:." */*.java 
-
-echo "Voy a correr ../ejemplos/22_errores_sintax.txt"
-java -cp "../cup.jar:." asint.Main  ../ejemplos/22_errores_sintax.txt
